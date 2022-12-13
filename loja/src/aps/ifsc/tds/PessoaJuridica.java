fed00a48edf06f9f8726a38c4d8e92ae3f20ee50
aluno <aluno@gw-c277>
2022-12-13 14:38:23 -0300

    Metedos e Get e Set na classe pessoajuridica

--- loja/src/aps/ifsc/tds/PessoaJuridica.java
+++ loja/src/aps/ifsc/tds/PessoaJuridica.java
@@ -1,5 +1,15 @@
 package aps.ifsc.tds;
 
-public class PessoaJuridica {
+public class PessoaJuridica extends Cliente {
+	
+	 private String cnpj;
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
 
 }
