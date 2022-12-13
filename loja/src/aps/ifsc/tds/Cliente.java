7c9810cc5c3ca4234acaa8f48104a53cbc2b0c83
aluno <aluno@172.16.3.42>
2022-12-13 14:42:03 -0300

    Metodos pessoa fisica e cliente criados e get and setters gerados

--- loja/src/aps/ifsc/tds/Cliente.java
+++ loja/src/aps/ifsc/tds/Cliente.java
@@ -1,5 +1,28 @@
 package aps.ifsc.tds;
 //kkkkkk
 public class Cliente {
+	
+	private int clienteID;
+	private String Nome;
+	private Endereco endereco;
+	
+	public int getClienteID() {
+		return clienteID;
+	}
+	public void setClienteID(int clienteID) {
+		this.clienteID = clienteID;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
 
 }
--- loja/src/aps/ifsc/tds/PessoaFisica.java
+++ loja/src/aps/ifsc/tds/PessoaFisica.java
@@ -1,5 +1,15 @@
 package aps.ifsc.tds;
 
 public class PessoaFisica {
+	
+	private String cpf;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 
 }
