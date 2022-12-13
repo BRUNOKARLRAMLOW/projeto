6381017e9d28920375be674b528d94641c7100c9
aluno <aluno@172.16.3.42>
2022-12-13 15:14:07 -0300

    metodo construtor criado em pessoaFisica e pessoaJuridica

--- loja/src/aps/ifsc/tds/PessoaFisica.java
+++ loja/src/aps/ifsc/tds/PessoaFisica.java
@@ -2,6 +2,11 @@ package aps.ifsc.tds;
 
 public class PessoaFisica extends Cliente{
 	
+	public PessoaFisica(int clienteID, String Nome, Endereco endereco, String cpf) {
+		super(clienteID, Nome, endereco);
+		this.cpf = cpf;
+	}
+
 	private String cpf;
 
 	public String getCpf() {
