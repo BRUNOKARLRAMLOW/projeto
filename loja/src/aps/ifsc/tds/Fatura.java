ab85350fb32ebe2f60427ec083227e7be53a1ef2
aluno <aluno@172.16.3.42>
2022-12-13 14:51:23 -0300

    get set e metodos fatura e UC

--- loja/src/aps/ifsc/tds/Fatura.java
+++ loja/src/aps/ifsc/tds/Fatura.java
@@ -1,5 +1,42 @@
 package aps.ifsc.tds;
 
 public class Fatura {
+	
+	private int faturaID;
+	private int mesAno;
+	private UC unidadeConsumidora;
+	private int consumo;
+	private boolean pago;
+	
+	public int getFaturaID() {
+		return faturaID;
+	}
+	public void setFaturaID(int faturaID) {
+		this.faturaID = faturaID;
+	}
+	public int getMesAno() {
+		return mesAno;
+	}
+	public void setMesAno(int mesAno) {
+		this.mesAno = mesAno;
+	}
+	public UC getUnidadeConsumidora() {
+		return unidadeConsumidora;
+	}
+	public void setUnidadeConsumidora(UC unidadeConsumidora) {
+		this.unidadeConsumidora = unidadeConsumidora;
+	}
+	public int getConsumo() {
+		return consumo;
+	}
+	public void setConsumo(int consumo) {
+		this.consumo = consumo;
+	}
+	public boolean isPago() {
+		return pago;
+	}
+	public void setPago(boolean pago) {
+		this.pago = pago;
+	}
 
 }
--- loja/src/aps/ifsc/tds/UC.java
+++ loja/src/aps/ifsc/tds/UC.java
@@ -1,5 +1,42 @@
 package aps.ifsc.tds;
 
 public class UC {
+	
+	private int ucid;
+	private Cliente cliente;
+	private int leituraAnterior;
+	private int leituraAtual;
+	private Endereco endereco;
+	
+	public int getUcid() {
+		return ucid;
+	}
+	public void setUcid(int ucid) {
+		this.ucid = ucid;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public int getLeituraAnterior() {
+		return leituraAnterior;
+	}
+	public void setLeituraAnterior(int leituraAnterior) {
+		this.leituraAnterior = leituraAnterior;
+	}
+	public int getLeituraAtual() {
+		return leituraAtual;
+	}
+	public void setLeituraAtual(int leituraAtual) {
+		this.leituraAtual = leituraAtual;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
 
 }
