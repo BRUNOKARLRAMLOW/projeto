e32b80f9a0dcb7ac9bc363176dc0271ed38ca8ed
aluno <aluno@172.16.3.42>
2022-12-13 14:27:16 -0300

    Metodo Private criado e Get and setters

--- loja/src/aps/ifsc/tds/Endereco.java
+++ loja/src/aps/ifsc/tds/Endereco.java
@@ -1,5 +1,40 @@
 package aps.ifsc.tds;
 
 public class Endereco {
+	private String rua;
+	private int numero;
+	private String cidade;
+	private String cep;
+	private String estado;
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
 
 }
