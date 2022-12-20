bcfcff6d137fb0686a0c09853cc1fb61aa045f66
aluno <aluno@172.16.3.31>
2022-12-20 14:58:22 -0300

    criado tarifa

---
+++ loja/src/aps/ifsc/tds/Tarifa.java
@@ -0,0 +1,47 @@
+package aps.ifsc.tds;
+
+public class Tarifa {
+	private double residencial1;
+	private double residencial2;
+	private double comercial1;
+	private double comercial2;
+	private double icms;
+	private double taxailuminacaoPublica;
+	public double getResidencial1() {
+		return residencial1;
+	}
+	public void setResidencial1(double residencial1) {
+		this.residencial1 = residencial1;
+	}
+	public double getResidencial2() {
+		return residencial2;
+	}
+	public void setResidencial2(double residencial2) {
+		this.residencial2 = residencial2;
+	}
+	public double getComercial1() {
+		return comercial1;
+	}
+	public void setComercial1(double comercial1) {
+		this.comercial1 = comercial1;
+	}
+	public double getComercial2() {
+		return comercial2;
+	}
+	public void setComercial2(double comercial2) {
+		this.comercial2 = comercial2;
+	}
+	public double getIcms() {
+		return icms;
+	}
+	public void setIcms(double icms) {
+		this.icms = icms;
+	}
+	public double getTaxailuminacaoPublica() {
+		return taxailuminacaoPublica;
+	}
+	public void setTaxailuminacaoPublica(double taxailuminacaoPublica) {
+		this.taxailuminacaoPublica = taxailuminacaoPublica;
+	}
+	
+}
